44f840d7b025e4a5849aca85544f71da0c1d17ef
psaccul <psaccul@hotmail.com>
2022-04-24 21:32:50 -0300

    Clase Js-1 de la carpeta 02 terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -172,7 +172,7 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
  if(numero > 0){
-   reurn "Es positivo";
+   return "Es positivo";
  }else if(numero < 0){
    return "Es negativo"
  }else{ //// numero === 0
